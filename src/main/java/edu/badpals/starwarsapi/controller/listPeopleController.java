6e08c66ee9f49b8498e0c7f20de16a99d26c95b3
a23sergiopg <serprigar@gmail.com>
2024-10-18 14:23:58 +0200

    feat(listPeopleController.java): add people list controller

---
+++ src/main/java/edu/badpals/starwarsapi/controller/listPeopleController.java
@@ -0,0 +1,42 @@
+package edu.badpals.starwarsapi.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class listPeopleController implements Initializable {
+    @FXML
+    private Button btnVolver;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    @FXML
+    public void toPeopleSearch(ActionEvent event) {
+
+        try {
+            // Cargar la nueva vista desde el archivo FXML
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/mainView.fxml"));
+            Parent root = loader.load();
+
+            // Obtener la ventana actual (stage) y cambiar la escena
+            Stage stage = (Stage) btnVolver.getScene().getWindow();
+            stage.setScene(new Scene(root));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
