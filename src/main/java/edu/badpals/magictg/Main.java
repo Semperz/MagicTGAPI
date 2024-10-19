db2ee2571444ece7e065a27c9f1cc50b77651103
Manuerty <manuelcendonr@gmail.com>
2024-10-19 15:02:44 +0200

    feat(Main): fix the vbox view

--- src/main/java/edu/badpals/magictg/Main.java
+++ src/main/java/edu/badpals/magictg/Main.java
@@ -3,6 +3,11 @@ package edu.badpals.magictg;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundSize;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,14 +16,12 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/mainView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1056, 716);
+        VBox root = fxmlLoader.load(); // Cargar el VBox desde FXML
+        Scene scene = new Scene(root, 1056, 716);
         stage.setTitle("¡¡Magic The Gathering Api!!");
         stage.setScene(scene);
         stage.setMinWidth(1056);
         stage.setMinHeight(716);
-        stage.centerOnScreen();
-        stage.setFullScreenExitHint(""); // Elimina el mensaje de salida
-        stage.setFullScreen(true); // Cambiar a pantalla completa automáticamente
         stage.show();
     }
 
