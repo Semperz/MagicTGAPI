2230d83a5690843b1e9e7e30f6535a22ee390290
Sergio <serprigar@gmail.com>
2024-10-20 16:28:30 +0200

    feat(secondWindowController.java): add pop up window if a field is missing in exportWindow

--- src/main/java/edu/badpals/magictg/controller/secondWindowController.java
+++ src/main/java/edu/badpals/magictg/controller/secondWindowController.java
@@ -7,9 +7,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -51,19 +49,29 @@ public class secondWindowController implements Initializable {
 
     @FXML
     private void handleSaveAction() {
-        String fileName = fileNameField.getText();  // Obtener el nombre del archivo
-        String format = formatBox.getValue();       // Obtener el formato seleccionado
+        String fileName = fileNameField.getText();
+        String format = formatBox.getValue();
 
         if (fileName != null && !fileName.isEmpty() && format != null && apiData != null) {
             // Llamar a FileExporter para guardar los datos en el formato seleccionado
             DataExporter.exportData(fileName, format, apiData);
         } else {
-            throw new RuntimeException();
+            badExportAlert(Alert.AlertType.ERROR, "Exportación fallida", "Faltan datos por cubrir o no seleccionaste una carta");
         }
+    }
+
+    private void badExportAlert(Alert.AlertType alertType, String title, String mesasge) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(mesasge);
+        alert.getButtonTypes().setAll(new ButtonType("Cerrar"));
+        alert.showAndWait();
     }
 
     public static void setApiData(String data) {
         apiData = data;
     }
+
 
 }
